2e0343e90e93650c008f1b0fdec7aea9fb672da3
BuddhiWathsala <b.wathsala.bw@gmail.com>
2016-11-30 13:09:05 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Vivo/';
+$base = null;
+if($_SERVER['HTTP_HOST']=='localhost')
+{
+  $base = 'http://localhost/Vivo/';
+}else {
+  $base = 'https://salty-citadel-93012.herokuapp.com/';
+}
+$config['base_url'] = $base;
 
 /*
 |--------------------------------------------------------------------------
